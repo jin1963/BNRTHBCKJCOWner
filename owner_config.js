4648b25dfda2838c95102e3dd93150c610aa4e44
KAOJNO DRAGON CHAIN <kaojino89@gmail.com>
2026-02-11 17:21:34 +0700

    Create owner_config.js

---
+++ owner_config.js
@@ -0,0 +1,11 @@
+// ====== CoreV4 + EarningsV2 CONFIG ======
+window.APP_CONFIG = {
+  CHAIN_ID_DEC: 56,
+  CHAIN_ID_HEX: "0x38",
+  CHAIN_NAME: "BNB Smart Chain",
+  BLOCK_EXPLORER: "https://bscscan.com",
+
+  // Contracts
+  CORE:     "0x66425FB7dAD286086b9fF024bf1e92A41926A50e", // CoreV4 (ล่าสุด)
+  EARNINGS: "0xf4e58b87909c68a07327ea3c82450D2Db51e6f0C", // EarningsV2 (ของคุณ)
+};
